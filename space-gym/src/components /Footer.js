a23df616ddc5a12fcf123aee4eb0f465be2f4106
pablotroncosog <pablo.troncoso@mail.udp.cl>
2022-11-08 23:24:45 +0000

    tercer commit

---
+++ space-gym/src/components /Footer.js
@@ -0,0 +1,61 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css"
+
+
+export function Footer() {
+
+    return   <div className="container-fluid">
+    <div className="row mt-4 p-5">
+        <div className="col-xs-12 col-md-6 col-lg-3 bg-dark text-light">
+            <p className="h4 text-danger"> </p>
+            <ul className="list-group list-group-flush">
+                <li>
+                    <p> a </p>
+                </li>
+                <li>
+                    <p> a </p>
+                </li>
+            </ul>
+        </div>
+        <div className="col-xs-12 col-md-6 col-lg-3 bg-dark text-light">
+            <p className="h4 text-danger">  </p>
+            <ul className="list-group list-group-flush">
+                <li>
+                    <p> a </p>
+                </li>
+                <li>
+                    <p> a </p>
+                </li>
+            </ul>
+        </div>
+        <div className="col-xs-12 col-md-6 col-lg-3 bg-dark text-light">
+            <p className="h4 text-danger">  </p>
+            <ul className="list-group list-group-flush">
+                <li>
+                    <p> a </p>
+                </li>
+                <li>
+                    <p> a </p>
+                </li>
+            </ul>
+        </div>
+        <div className="col-xs-12 col-md-6 col-lg-3 bg-dark text-light">
+            <p className="h4 text-danger">  </p>
+            <ul className="list-group list-group-flush">
+                <li>
+                    <p> a </p>
+                </li>
+                <li>
+                    <p> a </p>
+                </li>
+            </ul>
+        </div>
+        <div className="col-12 text-light text-center mt-5"> </div>
+    </div>
+</div>
+
+
+
+};
+
+export default Footer;
