46d9dc1275b1af28b14595f68844f62ef810b8fb
Marycarmen Zambrano <zambranomarycarmen48@gmail.com>
2022-11-11 22:48:14 +0000

    se agrega input

--- space-gym/src/App.js
+++ space-gym/src/App.js
@@ -2,11 +2,13 @@ import './styles/App.css';
 import "bootstrap/dist/css/bootstrap.min.css"
 import { BrowserRouter, Routes, Route, Form } from "react-router-dom";
 import injectContext from "./store/Context.js";
+import Admin from './views/Admin';
 
 function App() {
   return (
     <div className="App">
-      <h1> Hola </h1>
+      <Admin/>
+      
      
     </div>
   );
---
+++ space-gym/src/views/Admin.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Admin = () => {
+    return(
+        <form>
+            <label for='name'></label>
+            <input type= 'text' name></input>
+        </form>
+    )
+};
+
+export default Admin; 
