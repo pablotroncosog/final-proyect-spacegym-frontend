94684b9da24d89aa04ef9c1d79d274b4bac6acd5
githubfra <diazcabellofrancisca@gmail.com>
2022-11-15 00:22:54 +0000

    cambios

--- space-gym/src/App.js
+++ space-gym/src/App.js
@@ -2,14 +2,14 @@ import './styles/App.css';
 import "bootstrap/dist/css/bootstrap.min.css"
 import { BrowserRouter, Routes, Route, Form } from "react-router-dom";
 import injectContext from "./store/Context.js";
+import Registration from "./views/Registration";
 
 function App() {
-  return (
-    <div className="App">
-      <h1> Hola </h1>
-     
-    </div>
-  );
-}
+  return  <BrowserRouter>
+              <Routes>
+                <Route path="/registration" element={<Registration />}/>
+              </Routes>
+          </BrowserRouter>
+}     
 
 export default App;
