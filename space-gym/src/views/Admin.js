5aa290696c51c73542d1a4dbd1d788ac6c50c10f
Marycarmen Zambrano <zambranomarycarmen48@gmail.com>
2022-11-15 02:57:07 +0000

    se modifica los input

--- space-gym/src/views/Admin.js
+++ space-gym/src/views/Admin.js
@@ -5,169 +5,227 @@ const Admin = () => {
     return (
 
 
-        <div class="container">
-            <h1 >Administracion</h1>
-            <form class="row g-3 ">
-                <div class="col-4">
-                    <label for="productInput" class="form-label">Nombre del Producto</label>
-                    <input type="product" class="form-control" text-size="100" id="productInput"></input>
+        /*Primer segmento*/
+        <div className="container">
+
+            <h1>Administracion</h1>
+
+            <form className="row g-4 ">
+                <div className="col-4">
+                    <label for="product" className="p-1 mb-1 bg-secondary bg-opacity-50 text-white form-label">Nombre del Producto</label>
+                    <input type="product" className="form-control" id="product" required></input>
                 </div>
 
-                <div class="col-6">
-                    <label for="imageInput" class="form-label">Imagen de Referencia</label>
-                    <input class="col-sm-6 col-md-5 offset-md-2 col-lg-6 offset-lg-0" type Submit="image" class="form-control" id="imageInput" ></input>
+                <div className="col-4">
+                    <label for="image" className="p-1 mb-1 bg-secondary bg-opacity-50 text-white form-label">Imagen de Referencia</label>
+                    <input type Submit="image" className="form-control" id="image" required></input>
+                </div>
+            </form >
+
+
+            <div className="row">
+                <div className="g-4 col-4">
+
+
+                    <label for="price" className=" p-1 mb-1 bg-secondary bg-opacity-50 text-white  form-label">Precio</label>
+                    <input type="price" className="form-control" id="price"></input>
                 </div>
 
-                <div class="col-4">
-                    <label for="priceInput" class="form-label">Precio</label>
-                    <input type="price" class="form-control" id="priceInput" ></input>
+                <div className="g-4 col-4">
+                    <label for="description" className=" p-1 mb-1 bg-secondary bg-opacity-50 text-white  form-label">Descripcion</label>
+                    <input type="description" className="form-control" id="description"></input>
                 </div>
 
-                <div class="col-6">
-                    <label for="descriptionInput" class="form-label">Descripcion del Producto</label>
-                    <input class="col-sm-6 col-md-5 offset-md-2 col-lg-6 offset-lg-0" type="description" class="form-control" id="descriptionInput" ></input>
-                </div>
-
-                <div class="col-4">
-                    <label for="productclassInput" class="form-label">Clase del Producto</label>
-                    <input type="product" class="form-control" id="productInput" ></input>
-                </div>
-                <div>
+                <div className="col-4">
                     <button type="button" className="btn-sm">Subir Producto</button>
                 </div>
+            </div>
 
 
 
 
-                <div class="row g-3">
-                    <div class="col-2">
-                        <label for="productnameInput" class="form-label">Nombre del Producto</label>
-                        <input type="productnameclass" class="form-control" id="productnameInput" ></input>
-                    </div>
-                    <div class="col-2">
-                        <label for="productclassInput" class="form-label">Precio</label>
-                        <input type="productclass" class="form-control" id="productclassInput" ></input>
-                    </div>
-                    <div class="col-2">
-                        <label for="productclassInput" class="form-label">Clase</label>
-                        <input type="productclass" class="form-control" id="productclassInput" ></input>
-                    </div>
-                    <div class="col-2">
-                        <label for="productclassInput" class="form-label">Descripcion</label>
-                        <input type="productclass" class="form-control" id="productclassInput" ></input>
-                    </div>
+            <div className="row">
+                <div className="g-4 col-4">
+                    <label for="productclass" className=" p-1 mb-1 bg-secondary bg-opacity-50 text-white form-label">Clase del Producto</label>
+                    <input type="productclass" className="form-control" id="productclass"></input>
+                </div>
+            </div>
 
-                    <div class="col-2">
-                    <input type="product" class="form-control" id="productInput" ></input>
-                    </div>
+
+            
 
 
 
-                    <div>
-                        <div class="row g-3">
-                            <div class="col-2">
-                                <button class="button" type="button" >Editar Producto</button>
-                            </div>
+
+        </div>
+
+
+
+   
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /* <div className="col-4">
+                <label for="priceInput" className="form-label">Precio</label>
+                <input type="price" className="form-control" id="priceInput" ></input>
+            </div>
+
+            <div className="col-6">
+                <label for="descriptionInput" className="form-label">Descripcion del Producto</label>
+                <input className="col-sm-6 col-md-5 offset-md-2 col-lg-6 offset-lg-0" type="description" className="form-control" id="descriptionInput" ></input>
+            </div>
+
+            <div className="col-4">
+                <label for="productclassNameInput" className="form-label">Clase del Producto</label>
+                <input type="product" className="form-control" id="productInput" ></input>
+            </div>
+            <div>
+                <button type="button" classNameName="btn-sm">Subir Producto</button>
+            </div>
+
+
+
+
+            <div className="row g-3">
+                <div className="col-2">
+                    <label for="productnameInput" className="form-label">Nombre del Producto</label>
+                    <input type="productnameclassName" className="form-control" id="productnameInput" ></input>
+                </div>
+                <div className="col-2">
+                    <label for="productclassNameInput" className="form-label">Precio</label>
+                    <input type="productclassName" className="form-control" id="productclassNameInput" ></input>
+                </div>
+                <div className="col-2">
+                    <label for="productclassNameInput" className="form-label">Clase</label>
+                    <input type="productclassName" className="form-control" id="productclassNameInput" ></input>
+                </div>
+                <div className="col-2">
+                    <label for="productclassNameInput" className="form-label">Descripcion</label>
+                    <input type="productclassName" className="form-control" id="productclassNameInput" ></input>
+                </div>
+
+                <div className="col-2">
+                    <input type="product" className="form-control" id="productInput" ></input>
+                </div>
+
+
+
+                <div>
+                    <div className="row g-3">
+                        <div className="col-2">
+                            <button className="button" type="button" >Editar Producto</button>
                         </div>
                     </div>
+                </div>
 
 
-                    <div class="row g-3">
-                        <div class="col-2">
-                            <label for="productnameInput" class="form-label">Nombre</label>
-                            <input type="productnameclass" class="form-control" id="productnameInput" ></input>
-                        </div>
-                        <div class="col-2">
-                            <label for="productclassInput" class="form-label">Celular</label>
-                            <input type="productclass" class="form-control" id="productclassInput" ></input>
-                        </div>
-                        <div class="col-2">
-                            <label for="productclassInput" class="form-label">Correo</label>
-                            <input type="productclass" class="form-control" id="productclassInput" ></input>
-                        </div>
-                        <div class="col-2">
-                            <label for="productclassInput" class="form-label">Region</label>
-                            <input type="productclass" class="form-control" id="productclassInput" ></input>
-                        </div>
-                        <div class="col-2">
-                            <label for="productclassInput" class="form-label">Calle</label>
-                            <input type="productclass" class="form-control" id="productclassInput" ></input>
-                        </div>
-
+                <div className="row g-3">
+                    <div className="col-2">
+                        <label for="productnameInput" className="form-label">Nombre</label>
+                        <input type="productnameclassName" className="form-control" id="productnameInput" ></input>
                     </div>
-
-
-                    <div class="row g-3">
-                        <div class="col-2">
-                            <input type="productclass" class="form-control" id="productclassInput" ></input>
-                            </div>
-                            <div class="col-2">
-                            <input type="productclass" class="form-control" id="productclassInput" ></input>
-                            </div>
-                            <div class="col-2">
-                            <input type="productclass" class="form-control" id="productclassInput" ></input>
-                            </div>
-                            <div class="col-2">
-                            <input type="productclass" class="form-control" id="productclassInput" ></input>
-                            </div>
-                            <div class="col-2">
-                            <input type="productclass" class="form-control" id="productclassInput" ></input>
-                            </div>
-                            <div class="col-2">
-                            <button class="button" type="button" >Editar</button>
-                            </div>
+                    <div className="col-2">
+                        <label for="productclassNameInput" className="form-label">Celular</label>
+                        <input type="productclassName" className="form-control" id="productclassNameInput" ></input>
                     </div>
-
-
-
-                    <div class="row g-3">
-                        <div class="col-2">
-                            <input type="productclass" class="form-control" id="productclassInput" ></input>
-                            </div>
-                            <div class="col-2">
-                            <input type="productclass" class="form-control" id="productclassInput" ></input>
-                            </div>
-                            <div class="col-2">
-                            <input type="productclass" class="form-control" id="productclassInput" ></input>
-                            </div>
-                            <div class="col-2">
-                            <input type="productclass" class="form-control" id="productclassInput" ></input>
-                            </div>
-                            <div class="col-2">
-                            <input type="productclass" class="form-control" id="productclassInput" ></input>
-                            </div>
+                    <div className="col-2">
+                        <label for="productclassNameInput" className="form-label">Correo</label>
+                        <input type="productclassName" className="form-control" id="productclassNameInput" ></input>
                     </div>
-
-
-                    <div class="row g-3">
-                        <div class="col-2">
-                            <input type="productclass" class="form-control" id="productclassInput" ></input>
-                            </div>
-                            <div class="col-2">
-                            <input type="productclass" class="form-control" id="productclassInput" ></input>
-                            </div>
-                            <div class="col-2">
-                            <input type="productclass" class="form-control" id="productclassInput" ></input>
-                            </div>
-                            <div class="col-2">
-                            <input type="productclass" class="form-control" id="productclassInput" ></input>
-                            </div>
-                            <div class="col-2">
-                            <input type="productclass" class="form-control" id="productclassInput" ></input>
-                            </div>
-                            <div class="col-2">
-                            <button class="button" type="button" >Eliminar</button>
-                            </div>
+                    <div className="col-2">
+                        <label for="productclassNameInput" className="form-label">Region</label>
+                        <input type="productclassName" className="form-control" id="productclassNameInput" ></input>
+                    </div>
+                    <div className="col-2">
+                        <label for="productclassNameInput" className="form-label">Calle</label>
+                        <input type="productclassName" className="form-control" id="productclassNameInput" ></input>
                     </div>
 
                 </div>
 
-            </form >
-        </div >
+
+                <div className="row g-3">
+                    <div className="col-2">
+                        <input type="productclassName" className="form-control" id="productclassNameInput" ></input>
+                    </div>
+                    <div className="col-2">
+                        <input type="productclassName" className="form-control" id="productclassNameInput" ></input>
+                    </div>
+                    <div className="col-2">
+                        <input type="productclassName" className="form-control" id="productclassNameInput" ></input>
+                    </div>
+                    <div className="col-2">
+                        <input type="productclassName" className="form-control" id="productclassNameInput" ></input>
+                    </div>
+                    <div className="col-2">
+                        <input type="productclassName" className="form-control" id="productclassNameInput" ></input>
+                    </div>
+                    <div className="col-2">
+                        <button className="button" type="button" >Editar</button>
+                    </div>
+                </div>
 
 
 
+                <div className="row g-3">
+                    <div className="col-2">
+                        <input type="productclassName" className="form-control" id="productclassNameInput" ></input>
+                    </div>
+                    <div className="col-2">
+                        <input type="productclassName" className="form-control" id="productclassNameInput" ></input>
+                    </div>
+                    <div className="col-2">
+                        <input type="productclassName" className="form-control" id="productclassNameInput" ></input>
+                    </div>
+                    <div className="col-2">
+                        <input type="productclassName" className="form-control" id="productclassNameInput" ></input>
+                    </div>
+                    <div className="col-2">
+                        <input type="productclassName" className="form-control" id="productclassNameInput" ></input>
+                    </div>
+                </div>
 
+
+                <div className="row g-3">
+                    <div className="col-2">
+                        <input type="productclassName" className="form-control" id="productclassNameInput" ></input>
+                    </div>
+                    <div className="col-2">
+                        <input type="productclassName" className="form-control" id="productclassNameInput" ></input>
+                    </div>
+                    <div className="col-2">
+                        <input type="productclassName" className="form-control" id="productclassNameInput" ></input>
+                    </div>
+                    <div className="col-2">
+                        <input type="productclassName" className="form-control" id="productclassNameInput" ></input>
+                    </div>
+                    <div className="col-2">
+                        <input type="productclassName" className="form-control" id="productclassNameInput" ></input>
+                    </div>
+                    <div className="col-2">
+                        <button className="button" type="button" >Eliminar</button>
+                    </div>
+                </div>
+
+
+
+            </div >*/
 
 
     )
