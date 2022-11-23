7b42f536fde18b850a288968f6562a3bf3d0d91a
pablotroncosog <pablo.troncoso@mail.udp.cl>
2022-11-23 20:12:35 +0000

    store

--- space-gym/src/App.js
+++ space-gym/src/App.js
@@ -1,21 +1,34 @@
 import React from 'react';
 import './styles/App.css';
 import "bootstrap/dist/css/bootstrap.min.css"
+import injectContext from "./store/Context.js";
+import { BrowserRouter, Routes, Route, Form } from "react-router-dom";
 /* import { BrowserRouter, Routes, Route, Form } from "react-router-dom"; */
 /* import injectContext from "./store/Context.js"; */
 
 import { Navbar } from "./components/Navbar"
-import { Footer } from "./components/Footer" 
+import { Footer } from "./components/Footer"
 import { Main } from "./views/Main"
+import { Offerts } from "./views/Offerts"
+import { Product } from './views/Product';
 
 function App() {
   return (
     <div className="App">
-      <Navbar /> 
-      <Main/> 
-      <Footer/>
+      <Navbar />
+      <Main />
+      <div className='col-10 g-4 mt-2 p-2'>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Offerts />}> </Route>
+            <Route path='/:id' element={<Product />}> </Route>
+          </Routes>
+        </BrowserRouter> 
+        </div>
+      
+      <Footer />
     </div>
   );
 }
 
-export default App;
+export default injectContext(App);
---
+++ space-gym/src/views/Offerts.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { useContext, useEffect } from "react";
+import { Link } from "react-router-dom"
+import { Context } from "../store/Context"
+import placeholderImg from "../images/placeholder-image.png"
+
+
+export function Offerts() {
+
+    const { store, actions } = useContext(Context);
+    useEffect(() => {
+        actions.getCharacters()
+    }, [])
+
+    return <div className="container-fluid">
+
+        <div className="row">
+            {store.products.map((products) => {
+                return (
+                    <div className="col-3 container-fluid d-flex justify-content-center">
+                        <div className= "card h-100 img-fluid">
+                            <img src= {placeholderImg} className="card-img-center" alt="..." />
+                            <div className="card-body">
+                            <Link to={"/" + products.uid} > <h5 className="card-title text-align-center"> {products.name} </h5>  </Link>
+                                <p className="card-text text-align-center"> <h3> ${products.price} </h3> </p>
+                            </div>
+                            <div className="card-footer container-fluid d-flex justify-content-center">
+                                <button type="button" className="btn btn-primary btn-lg mx-auto ">Agregar al carrito</button>
+                            </div>
+                        </div>
+                    </div> 
+                )
+            })}
+        </div>
+
+    </div>
+
+
+
+};
+
+export default Offerts;
