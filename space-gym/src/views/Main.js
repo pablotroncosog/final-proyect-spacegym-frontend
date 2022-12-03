959330515a8b0351e9dd137ff50731ab01944f0e
githubfra <diazcabellofrancisca@gmail.com>
2022-12-03 00:38:17 +0000

    changes2

--- space-gym/src/views/Main.js
+++ space-gym/src/views/Main.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "../styles/App.css"
-import { useState } from "react";
+
 
 import bicep from "../images/bicep.jpg";
 import deadlift from "../images/deadlift.jpg"
@@ -10,45 +10,15 @@ import MMA from "../images/MMA.jpg"
 import Yoga from "../images/Yoga.jpg"
 
 export function Main() {
-    const [inputFiles, setInputFiles] = useState(null);
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        const formData = new FormData();
-        formData.append("file",inputFiles[0]);
-        console.log(formData.get("file"));
-        fetch(
-            "https://8080-4geeksacademy-htmlhello-vsscjfnzcdo.ws-us77.gitpod.io/upload_image",
-            {
-                method: "POST",
-                body: formData,               
-            }
-        )
-            .then((res) => res.json())
-            .then((data) => console.log(data))
-            .catch((error) => console.log(error));
-
-    };
-    const handleChange = (e) => {
-        setInputFiles(e.target.files);
-    };
+   
 
 
 
 
     return (
     
-    <><div className="g-4 col-4">
-            <form onSubmit={handleSubmit}>
-                <label for="kettlebell">Imagen de referencia:</label>
-                <input
-                    type="file"
-                    id="barra"
-                    name="barra"
-                    onChange={handleChange}
-                    accept="image/png, image/jpeg"></input>
-                <button className="btn btn-primary" type="submit">Upload</button>
-            </form>
-        </div><div className="container-fluid">
+    <>
+        <div className="container-fluid">
                 <div className="row">
                     <div className="col-4"> </div>
                     <div className="col-4 mt-5">  <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="true">
