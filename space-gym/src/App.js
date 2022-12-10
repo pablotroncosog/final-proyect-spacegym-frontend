9184d3457618e35fec3e56f5a981c2478ed3ff7d
githubfra <diazcabellofrancisca@gmail.com>
2022-12-10 00:26:45 +0000

    pul

--- space-gym/src/App.js
+++ space-gym/src/App.js
@@ -1,35 +1,23 @@
 import React from 'react';
 import './styles/App.css';
 import "bootstrap/dist/css/bootstrap.min.css"
-<<<<<<< HEAD
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-=======
 import 'bootstrap/dist/js/bootstrap.js';
-import { BrowserRouter, Routes, Route} from "react-router-dom";
->>>>>>> 5cf2abbfe77373924450fa836fac4aa2928b7243
+
 import injectContext from "./store/Context";
-
-
 import { Navbar } from "./components/Navbar"
 import { Footer } from "./components/Footer"
 import { NavbarUser } from './components/NavbarUser';
-
 import { Main } from "./views/Main"
-
-<<<<<<< HEAD
 import Login from "./views/Login";
 import Admin from './views/Admin';
 import Registration from "./views/Registration";
-=======
 import { Product } from './views/Product';
-import Login from "./views/Login";
-import Admin from './views/Admin';
-import Registration from "./views/Registration";
+
 import Account from './views/Account';
 import Shopping from './views/Shopping';
 import Combat from './views/Combat';
 
->>>>>>> 5cf2abbfe77373924450fa836fac4aa2928b7243
 
 
 function App() {
@@ -49,13 +37,10 @@ function App() {
           <Route path="/registration" element={<Registration />} />
           <Route path="/login" element={<Login />} />
           <Route path="/admin" element={<Admin />} />
-<<<<<<< HEAD
          
-=======
           <Route path="/account" element={<Account />} />
           <Route path="/shopping" element={<Shopping/>} />
           <Route path="/combat" element={<Combat/>} />
->>>>>>> 5cf2abbfe77373924450fa836fac4aa2928b7243
         </Routes>
       </BrowserRouter>
       <div className='col-12'>
